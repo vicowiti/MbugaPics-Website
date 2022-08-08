8dd22907d43c39ac71d8b9b1cad2ed719f5a1208
Victor Owiti <viowiti12@gmail.com>
2022-08-08 11:38:24 +0300

    added the hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Hero = () => {
-  return <div>Hero</div>;
+const Hero = ({ heading, message }) => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img w-full">
+      {/* Image Overlay */}
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/70 z[2] " />
+      <div className="p-5 text-white z-[2] lg:ml-[-10em] lg:mt-[-10em]">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="py-5 text-xl">{message}</p>
+        <button className="px-8 border ">Book Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
