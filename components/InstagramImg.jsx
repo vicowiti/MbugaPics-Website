3f93a530e8782aa09b116818af297cb1be2c124f
Victor Owiti <viowiti12@gmail.com>
2022-08-19 18:07:45 +0300

    deployment fix

--- components/InstagramImg.jsx
+++ components/InstagramImg.jsx
@@ -13,7 +13,11 @@ const InstagramImg = ({ socialImage }) => {
       />
       <div className="group flex justify-center items-center absolute top-0 bottom-0 left-0 right-0 hover:bg-black/70">
         <p className="text-gray-300 hidden group-hover:block">
-          <a href="https://www.instagram.com/veekdev/" target="_blank">
+          <a
+            href="https://www.instagram.com/veekdev/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaInstagram size={25} />
           </a>
         </p>
